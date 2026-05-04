9f094201bef3fbd47c1e59d3e091ec1a31cf36cc
yelloweyesfilms <132815974+yelloweyesfilms@users.noreply.github.com>
2026-05-04 11:48:28 +0200

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
-  api: {
-    bodyParser: false,
-  },
 }
+
+module.exports = nextConfig
